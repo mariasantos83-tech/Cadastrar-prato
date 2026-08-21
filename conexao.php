ddb4d6f0d21ccd1b3173d9a432d953c5895613a1
Gabriela_Harger <gabriela_h_dias@estudante.sesisenai.org.br>
2026-08-21 11:00:44 -0300

    add

--- conexao.php
+++ conexao.php
@@ -6,7 +6,6 @@ $senha = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8mb4", $usuario, $senha);
-    // Configura o PDO para lançar exceções em caso de erro
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão com o banco de dados: " . $e->getMessage());
